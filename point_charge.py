fcefefefa1e10ee5edf19ad58f20fcd8cabe46d6
evdedd <38300521+evdedd@users.noreply.github.com>
2018-04-11 15:43:00 -0700

    Remove more spaces

--- point_charge.py
+++ point_charge.py
@@ -10,25 +10,13 @@ x, y, z = np.meshgrid(np.arange(-1, 1.2, 0.4),
                       np.arange(-1, 1.2, 0.4),
                       np.arange(-1, 1.2, 0.4))
 
-
-
 # Make the direction data for the arrows
-
 u = x/(x**2+y**2+z**2)**(3/2)
-
 v = y/(x**2+y**2+z**2)**(3/2)
-
 w = z/(x**2+y**2+z**2)**(3/2)
 
-
-
 ax.quiver(x, y, z, u, v, w, length=0.05)
-
 ax.set_aspect('equal')
-
 ax.auto_scale_xyz([-1.0, 1.0], [-1.0, 1.0], [-1.0, 1.0])
 
-
-
 plt.show()
-
