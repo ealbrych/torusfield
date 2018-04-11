a4a3e75904da0a0d5f8c84b59d44d4b19717c62c
emil.albrych <ealbrych@berkeley.edu>
2018-04-11 15:32:31 -0700

    changes

--- point_charge.py
+++ point_charge.py
@@ -1,19 +1,44 @@
-
 from mpl_toolkits.mplot3d import axes3d
+
 import matplotlib.pyplot as plt
+
 import numpy as np
 
-fig = plt.figure()
+
+
+fig = plt.figure(figsize = (6, 6))
+
 ax = fig.gca(projection='3d')
 
-x, y, z = np.meshgrid(np.arange(-1, 1, 0.3),
-                      np.arange(-1, 1, 0.3),
-                      np.arange(-1, 1, 0.3))
+
+
+# Make the grid
+
+x, y, z = np.meshgrid(np.arange(-1, 1.2, 0.4),
+
+                      np.arange(-1, 1.2, 0.4),
+
+                      np.arange(-1, 1.2, 0.4))
+
+
+
+# Make the direction data for the arrows
 
 u = x/(x**2+y**2+z**2)**(3/2)
+
 v = y/(x**2+y**2+z**2)**(3/2)
+
 w = z/(x**2+y**2+z**2)**(3/2)
 
-ax.quiver(x, y, z, u, v, w, length=0.1)
+
+
+ax.quiver(x, y, z, u, v, w, length=0.05)
+
+ax.set_aspect('equal')
+
+ax.auto_scale_xyz([-1.0, 1.0], [-1.0, 1.0], [-1.0, 1.0])
+
+
 
 plt.show()
+
