ab5e9fe4ad6ab3d574540520bd9dec3b026521e4
evdedd <38300521+evdedd@users.noreply.github.com>
2018-04-13 15:03:59 -0700

    Rectangular prism

--- Cube.py
+++ Cube.py
@@ -11,9 +11,15 @@ b = np.arange(-1, 1.1, 0.4)
 c = np.arange(-1, 1.1, 0.4)
 i, j, k = np.meshgrid(a, b, c)
 
-xside = 2
-yside = 2
+q = 1
+
+xside = 1
+yside = 1
 zside = 1
+
+xstep = 10
+ystep = 10
+zstep = 10
 
 outside = (i < -xside/2) | (i > xside/2) | (j < -yside/2) | (j > yside/2) | (k < -zside/2) | (k > zside/2)
 
@@ -23,24 +29,24 @@ z = k[outside]
 
 def u(x,y,z):
     x_sum = 0
-    for a in np.arange(-xside/2,xside/2, 0.1):
-        for b in np.arange(-yside/2,yside/2, 0.1):
-            for c in np.arange(-zside/2,zside/2, 0.1):
-                x_sum += xside * yside * zside * 0.001*(x-a)/((x-a)**2+(y-b)**2+(z-c)**2)**(3/2)
+    for a in np.arange(-xside/2,xside/2, xside/xstep):
+        for b in np.arange(-yside/2,yside/2, yside/ystep):
+            for c in np.arange(-zside/2,zside/2, zside/zstep):
+                x_sum += q / (xstep * ystep * zstep) * (x-a)/((x-a)**2+(y-b)**2+(z-c)**2)**(3/2)
     return x_sum
 def v(x,y,z):
     y_sum = 0
-    for a in np.arange(-xside/2,xside/2, 0.1):
-        for b in np.arange(-yside/2,yside/2, 0.1):
-            for c in np.arange(-zside/2,zside/2, 0.1):
-                y_sum += xside * yside * zside * 0.001*(y-b)/((x-a)**2+(y-b)**2+(z-c)**2)**(3/2)
+    for a in np.arange(-xside/2,xside/2, xside/xstep):
+        for b in np.arange(-yside/2,yside/2, yside/ystep):
+            for c in np.arange(-zside/2,zside/2, zside/zstep):
+                y_sum += q / (xstep * ystep * zstep) * (y-b)/((x-a)**2+(y-b)**2+(z-c)**2)**(3/2)
     return y_sum
 def w(x,y,z):
     z_sum = 0
-    for a in np.arange(-xside/2,xside/2, 0.1):
-        for b in np.arange(-yside/2,yside/2, 0.1):
-            for c in np.arange(-zside/2,zside/2, 0.1):
-                z_sum += xside * yside * zside * 0.001*(z-c)/((x-a)**2+(y-b)**2+(z-c)**2)**(3/2)
+    for a in np.arange(-xside/2,xside/2, xside/xstep):
+        for b in np.arange(-yside/2,yside/2, yside/ystep):
+            for c in np.arange(-zside/2,zside/2, zside/zstep):
+                z_sum += q / (xstep * ystep * zstep) * (z-c)/((x-a)**2+(y-b)**2+(z-c)**2)**(3/2)
     return z_sum
 
 # Make the direction data for the arrows
@@ -48,7 +54,7 @@ u = u(x,y,z)
 v = v(x,y,z)
 w = w(x,y,z)
 
-ax.quiver(x, y, z, u, v, w, length=0.05)
+ax.quiver(x, y, z, u, v, w, length=0.1)
 
 ax.set_aspect('equal')
 ax.auto_scale_xyz([-1.0, 1.0], [-1.0, 1.0], [-1.0, 1.0])
