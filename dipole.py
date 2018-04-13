1a1a0fd390b49664ad572c9cf2d26b263b1143d0
evdedd <38300521+evdedd@users.noreply.github.com>
2018-04-13 14:16:58 -0700

    cube and dipole updates

--- dipole.py
+++ dipole.py
@@ -6,14 +6,18 @@ fig = plt.figure(figsize = (6, 6))
 ax = fig.gca(projection='3d')
 
 # Make the grid
-x, y, z = np.meshgrid(np.arange(-2.0, 2.2, 0.8),
-                      np.arange(-2.0, 2.2, 0.8),
-                      np.arange(-2.0, 2.2, 0.8))
+x, y, z = np.meshgrid(np.arange(-2.0, 2.2, 0.5),
+                      np.arange(-2.0, 2.2, 0.5),
+                      np.arange(-2.0, 2.2, 0.5))
+
+q1 = 1
+q2 = -1
+dist = 2
 
 # Make the direction data for the arrows
-u = x/(x**2+y**2+(z-1)**2)**(3/2)-x/(x**2+y**2+(z+1)**2)**(3/2)
-v = y/(x**2+y**2+(z-1)**2)**(3/2)-y/(x**2+y**2+(z+1)**2)**(3/2)
-w = (z-1)/(x**2+y**2+(z-1)**2)**(3/2)-(z+1)/(x**2+y**2+(z+1)**2)**(3/2)
+u = q1*x/(x**2+y**2+(z-dist/2)**2)**(3/2) + q2*x/(x**2+y**2+(z+dist/2)**2)**(3/2)
+v = q1*y/(x**2+y**2+(z-dist/2)**2)**(3/2) + q2*y/(x**2+y**2+(z+dist/2)**2)**(3/2)
+w = q1*(z-dist/2)/(x**2+y**2+(z-dist/2)**2)**(3/2) + q2*(z+dist/2)/(x**2+y**2+(z+dist/2)**2)**(3/2)
 
 ax.quiver(x, y, z, u, v, w, length=0.2)
 
