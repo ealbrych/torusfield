f1f78741b4c50ca3a97ab7fdbed2b32f663ae966
emil.albrych <ealbrych@berkeley.edu>
2018-04-17 22:37:49 -0700

    animation working
    Point charge animation working

--- point_animation.py
+++ point_animation.py
@@ -9,7 +9,7 @@ fig = plt.figure(figsize = (6, 6))
 ax = fig.add_subplot(1,1,1, projection = '3d')
 q0=1
 axcolor = 'lightgoldenrodyellow'
-axamp = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+axamp = plt.axes([0.25, 0.01, 0.65, 0.03], facecolor=axcolor)
 samp = Slider(axamp, 'Charge', 0.2, 2.0, valinit=q0)
 
 
@@ -29,8 +29,13 @@ ax.quiver(x, y, z, u, v, w, length=0.05)
 
 def update(val):
     q = samp.val
+    ax.clear()
     ax.quiver(x, y, z, q*u, q*v, q*w, length=0.05)
+    ax.set_aspect('equal')
+    ax.scatter(0,0,0, s=40, c='red')
+    ax.auto_scale_xyz([-1.0, 1.0], [-1.0, 1.0], [-1.0, 1.0])
     fig.canvas.draw_idle()
+    #fig.draw()
 samp.on_changed(update)
 
 ax.scatter(0,0,0, s=40, c='red')
