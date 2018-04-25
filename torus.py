5e76d3ecc36edfbb83dee5cc16bacdc79eb2074e
evdedd <38300521+evdedd@users.noreply.github.com>
2018-04-24 23:51:19 -0700

    Torus Update

--- torus.py
+++ torus.py
@@ -9,19 +9,26 @@ ax = fig.gca(projection='3d')
 # ----- CHARGE PARAMETERS -----
 q = 1                  # total charge
 
-b = 0.2
 a = 0.5
+b = 0.2
 
-step_size = 0.01        # size of step
+step_size_r = 0.04        # size of step
+step_size_theta = 2*np.pi/100
+step_size_h = 0.04
 
 # ----- WINDOW PARAMETERS -----
 window_size = 1        # window extends from -window_size to window_size
 
 # ----- VECTOR PARAMETERS -----
-vector_spacing = 0.4   # distance between vectors
-arrow_length = 0.2     # length scale for arrows
+vector_spacing = 0.5   # distance between vectors
+arrow_length = 0.1     # length scale for arrows
 
-
+# draw torus
+u, v = np.mgrid[0:2*np.pi:20j, 0:2*np.pi:20j]
+x = np.cos(u) * (a + b*np.cos(v))
+y = np.sin(u) * (a + b*np.cos(v))
+z = b*np.sin(v)
+ax.plot_wireframe(x, y, z, color="r")
 
 # ----- VECTOR PLOT -----
 
@@ -31,9 +38,12 @@ m = np.arange(-window_size, window_size+0.1, vector_spacing)
 n = np.arange(-window_size, window_size+0.1, vector_spacing)
 i, j, k = np.meshgrid(l, m, n)
 
-x=i
-y=j
-z=k
+# Remove object volume from meshgrid
+outside = np.sqrt((np.sqrt(i**2 + j**2) - a)**2 + k**2) > b
+
+x = i[outside]
+y = j[outside]
+z = k[outside]
 
 density = q / (np.pi**2 * 2 * b**2 * a)
 
@@ -42,12 +52,12 @@ def V(x, y, z):
     x_sum = 0
     y_sum = 0
     z_sum = 0
-    for h in np.arange(-b, b, step_size):
-        for theta in np.arange(0, 2*np.pi, step_size):
-            for r in np.arange(a-np.sqrt(b**2-h**2), a+np.sqrt(b**2-h**2), step_size):
-                x_sum += density * step_size**3  * r * (x-r*np.cos(theta))/((x-r*np.cos(theta))**2+(y-r*np.sin(theta))**2+(z-h)**2)**(3/2)
-                y_sum += density * step_size**3 * r * (y-r*np.sin(theta))/((x-r*np.cos(theta))**2+(y-r*np.sin(theta))**2+(z-h)**2)**(3/2)
-                z_sum += density * step_size**3 * r * (z-h)/((x-r*np.cos(theta))**2+(y-r*np.sin(theta))**2+(z-h)**2)**(3/2)
+    for h in np.arange(-b, b, step_size_h):
+        for theta in np.arange(0, 2*np.pi, step_size_theta):
+            for r in np.arange(a-np.sqrt(b**2-h**2), a+np.sqrt(b**2-h**2), step_size_r):
+                x_sum += density * step_size_h * step_size_r * step_size_theta * r * (x-r*np.cos(theta))/((x-r*np.cos(theta))**2+(y-r*np.sin(theta))**2+(z-h)**2)**(3/2)
+                y_sum += density * step_size_h * step_size_r * step_size_theta * r * (y-r*np.sin(theta))/((x-r*np.cos(theta))**2+(y-r*np.sin(theta))**2+(z-h)**2)**(3/2)
+                z_sum += density * step_size_h * step_size_r * step_size_theta * r * (z-h)/((x-r*np.cos(theta))**2+(y-r*np.sin(theta))**2+(z-h)**2)**(3/2)
     return [x_sum, y_sum, z_sum]
 
 # Make the direction data for the arrows
